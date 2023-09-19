64b9cda4c1901577104a41812ceb5f2eb54b2389
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-09-19 13:11:14 +0100

    profile box created and populated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './Profile.css';
+import ProfilePic from '../images/Gorka-profile.jpg';
 
 function Profile() {
 
-  console.log("This app is working.")
-
   return (
-    <div>
-      <h1>First Project (During Le Wagon)</h1>
+    <div className="Profile-box">
+
+      <div class="wrapper">
+
+        <div class="main">
+          <h1>Gorka Echavarri</h1>
+          <h2>Web Developer</h2>
+          <p>Linkedin</p>
+          <p>Github</p>
+        </div>
+
+        <div class="aside aside-1"><img src={ProfilePic} alt="Gorkas profile pic" className="Profile-pic"/></div>
+
+        <p class="footer">Long text describing my path and my interests.</p>
+
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Projects() {
+
+  console.log("This app is working.")
+
+  return (
+    <React.Fragment>
+      <h1>All Projects box</h1>
+    </React.Fragment>
+  );
+}
+
+export default Projects;
